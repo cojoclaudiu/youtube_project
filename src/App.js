c841197b1e2acfdc9490e48733a9cc99ab24e0e1
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-06 19:03:41 +0300

    working on sidebar, updated small things

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Header from './components/Header/Header';
 import Categories from './components/Categories/Categories';
 import Sidebar from './components/Sidebar/Sidebar';
 
-const App = () => {
-  return (
-    <div className="mainWrapper">
-      <Header />
-      <Categories />
-      <Sidebar />
-    </div>
-  );
-};
+const App = () => (
+  <div className="mainWrapper">
+    <Header />
+    <Categories />
+    <Sidebar />
+  </div>
+);
 
 export default App;

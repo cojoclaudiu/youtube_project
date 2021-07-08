30f6cc8525888fa6660808c7f040265ac7962ceb
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-08 10:50:30 +0300

    var change netlify

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const youtube = axios.create({
-  baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=18&regionCode=US&key=${REACT_APP_YT_API_KEY}`,
+  baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${process.env.REACT_APP_YT_API_KEY}`,
 });
 
 export default youtube;

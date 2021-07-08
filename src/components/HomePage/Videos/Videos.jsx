a43a4beed208d45085af6ea4b90cc3782b3bdd36
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-08 14:40:37 +0300

    timeStamp video function --needs restructure and moved in separate file/folder

--- src/components/HomePage/Videos/Videos.jsx
+++ src/components/HomePage/Videos/Videos.jsx
@@ -14,16 +14,56 @@ const Videos = () => {
     ytData();
   }, []);
 
+  const durationStamp = (duration) => {
+    const durationArray = duration.match(/\d+/g).map(Number);
+    // let newHours = null;
+    let timeStamp = null;
+
+    if (durationArray.length === 1) {
+      const [sec] = durationArray;
+      if (sec <= 9) {
+        timeStamp = `0:0${sec}`;
+      } else {
+        timeStamp = `0:${sec}`;
+      }
+    }
+
+    if (durationArray.length === 2) {
+      const [min, sec] = durationArray;
+      if (sec <= 9) {
+        timeStamp = `${min}:0${sec}`;
+      } else {
+        timeStamp = `${min}:${sec}`;
+      }
+    }
+
+    if (durationArray.length === 3) {
+      const [hour, min, sec] = durationArray;
+      if (min <= 9 && sec <= 9) {
+        timeStamp = `${hour}:0${min}:0${sec}`;
+      } else if (min <= 9 && sec >= 9) {
+        timeStamp = `${hour}:0${min}:${sec}`;
+      } else if (min >= 9 && sec <= 9) {
+        timeStamp = `${hour}:${min}:0${sec}`;
+      } else {
+        timeStamp = `${hour}:${min}:${sec}`;
+      }
+    }
+
+    return `${timeStamp}`;
+  };
+
   return (
     <div className={styles.videosContainer}>
       {videos.map((video) => (
         <div key={video.id} className={styles.videoContainer}>
-          <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+          <figure>
+            <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+            <div>{durationStamp(video.contentDetails.duration)}</div>
+          </figure>
           <div className={styles.videoDetails}>
-            <h3>{video.snippet.title}</h3>
+            <h3 className={styles.videoTitle}>{video.snippet.title}</h3>
           </div>
-
-          {/* {console.log(video)} */}
         </div>
       ))}
     </div>

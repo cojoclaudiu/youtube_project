b5eb5223f050fd48b27dd2161da90fad05c77679
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-09 19:00:10 +0300

    video stats

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const youtube = axios.create({
+export const youtube = axios.create({
   baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${process.env.REACT_APP_YT_API_KEY}`,
 });
 
-export default youtube;
+export const youtubeVideo = (videoId) =>
+  axios.create({
+    baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${process.env.REACT_APP_YT_API_KEY}`,
+  });
+
--- src/components/VideoPage/VideoPage.jsx
+++ src/components/VideoPage/VideoPage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import { youtubeVideo } from 'api/youtube';
 import styles from './VideoPage.module.css';
 
 const VideoPage = () => {
   const videoId = useLocation().search.replace('?v=', '');
+  const [info, setInfo] = useState({
+    title: 'Title Loading...',
+    likes: 'Likes Loading...',
+    dislikes: 'Dislikes Loading...',
+    views: 'Views Loading...',
+  });
 
-  // useEffect(() => console.log(videoId), [videoId]);
+  useEffect(() => {
+    async function videoData() {
+      const request = await youtubeVideo(videoId).get(``);
+      setInfo({
+        title: request.data.items[0].snippet.title,
+        likes: request.data.items[0].statistics.likeCount,
+        dislikes: request.data.items[0].statistics.dislikeCount,
+        views: request.data.items[0].statistics.viewCount,
+      });
+    }
+
+    videoData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className={styles.playerContainer}>
       <iframe
@@ -18,6 +38,10 @@ const VideoPage = () => {
         src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0"
       />
+      <div>{info.title}</div>
+      <div>Like: {info.likes}</div>
+      <div>Dislike: {info.dislikes}</div>
+      <div>Views: {info.views}</div>
     </div>
   );
 };

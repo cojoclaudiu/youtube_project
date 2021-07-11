b8944a3adc495ca8f141f4aa7b32b4fff33a5968
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-12 01:25:10 +0300

    debouncing added

--- src/components/Search/SearchHeader.jsx
+++ src/components/Search/SearchHeader.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useMemo, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import debounce from 'lodash.debounce';
 import { AiOutlineSearch } from 'react-icons/ai';
 import styles from './SearchHeader.module.css';
 
 const SearchHeader = () => {
   const history = useHistory();
+  const location = useLocation().search;
+  console.log(location);
   const [query, setQuery] = useState('');
 
-  const onHandleChange = (e) => setQuery(e.target.value);
+  const onQueryChange = (e) => setQuery(e.target.value);
+  const debouncedQueryChanged = useMemo(() => debounce(onQueryChange, 100), []);
+  useEffect(() => () => debouncedQueryChanged.cancel(), [debouncedQueryChanged]);
 
-  const handleSubmit = (e) => {
+  const onQuerySubmit = (e) => {
     e.preventDefault();
     history.push(`results?search=${query}`);
   };
 
   return (
     <div className={styles.headerContainerSearch}>
-      <form autoComplete="off" className={styles.searchForm} onSubmit={handleSubmit}>
+      <form autoComplete="off" className={styles.searchForm} onSubmit={onQuerySubmit}>
         <input
+          id="searchInput"
           name="searchInput"
           className={styles.searchInput}
           type="search"
           placeholder="Search"
-          onChange={onHandleChange}
+          onChange={debouncedQueryChanged}
         />
         <button type="button" className={styles.searchBtn}>
           <AiOutlineSearch />

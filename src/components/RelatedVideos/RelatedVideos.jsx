c012d31e5e0bf8d368a56ab1cf0b409027b6ef51
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-11 15:59:05 +0300

    timeStamp added on related videos

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { relatedVideos } from 'api/youtube';
+import { relatedVideos, youtubeVideo } from 'api/youtube';
+import durationStamp from 'helpers/durationStamp';
 import styles from './RelatedVideos.module.css';
 
 const RelatedVideos = ({ addId }) => {
   const [related, setRelated] = useState([]);
+  const [videoDuration, setVideoDuration] = useState([]);
 
   useEffect(() => {
     async function getRelatedVideos() {
@@ -18,17 +20,36 @@ const RelatedVideos = ({ addId }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [addId]);
 
+  useEffect(() => {
+    async function getVideoData() {
+      const arr = related.map(async (video) => {
+        const vId = video.id.videoId;
+        const response = await youtubeVideo(vId).get();
+        return durationStamp(response.data.items[0].contentDetails.duration);
+      });
+      const resolved = await Promise.all(arr);
+      setVideoDuration(...[resolved]);
+    }
+    getVideoData();
+  }, [related]);
 
+  console.log(videoDuration);
 
   return (
     <div className={styles.sidebarRelated}>
-      {related.map((video) => (
+      {related.map((video, index) => (
         <Link key={video.id.videoId} to={`/watch?v=${video.id.videoId}`}>
           <div className={styles.videoContainer}>
             <div className={styles.videoThumbnail}>
-              <img alt={video.snippet.id} src={video.snippet.thumbnails.medium.url} />
+              <div className={styles.thumbnailContainer}>
+                <img
+                  className={styles.thumbnailImage}
+                  alt={video.snippet.id}
+                  src={video.snippet.thumbnails.medium.url}
+                />
+                <div className={styles.timeStamp}>{videoDuration[index]}</div>
+              </div>
               <h3>{video.snippet.title}</h3>
-              <div>Duration: </div>
             </div>
           </div>
         </Link>

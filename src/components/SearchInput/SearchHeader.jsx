aaeedd8b6001e65fe0668055ff2bc94a8f20860c
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-12 18:00:48 +0300

    sidebar header

---
+++ src/components/SearchInput/SearchHeader.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useMemo, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import debounce from 'lodash.debounce';
+import { AiOutlineSearch } from 'react-icons/ai';
+import styles from './SearchHeader.module.css';
+
+const SearchHeader = () => {
+  const history = useHistory();
+
+  const [query, setQuery] = useState('');
+
+  const onQueryChange = (e) => setQuery(e.target.value);
+  const debouncedQueryChanged = useMemo(() => debounce(onQueryChange, 100), []);
+  useEffect(() => () => debouncedQueryChanged.cancel(), [debouncedQueryChanged]);
+
+  const onQuerySubmit = (e) => {
+    e.preventDefault();
+    history.push(`results?search=${query}`);
+  };
+
+  return (
+    <div className={styles.headerContainerSearch}>
+      <form autoComplete="off" className={styles.searchForm} onSubmit={onQuerySubmit}>
+        <input
+          id="searchInput"
+          name="searchInput"
+          className={styles.searchInput}
+          type="search"
+          placeholder="Search"
+          onChange={debouncedQueryChanged}
+        />
+        <button type="button" className={styles.searchBtn}>
+          <AiOutlineSearch />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchHeader;

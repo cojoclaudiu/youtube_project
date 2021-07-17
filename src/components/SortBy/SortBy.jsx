b12f40c792dbe6c173e959eee3cd452dcb380421
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-18 00:40:19 +0300

    add on change event on sort by

--- src/components/SortBy/SortBy.jsx
+++ src/components/SortBy/SortBy.jsx
@@ -7,13 +7,28 @@ export default function SortBy({ handleFilterASC, handleFilterDESC, handleFilter
       Sort trending by:
       <select className={styles.sortByItemsContainer} name="trending" id="trending">
         <optgroup label="Views">
-          <option value="Default" onSelect={handleFilterDefault} onTouchEnd={handleFilterDefault} onClick={handleFilterDefault}>
+          <option
+            value="Default"
+            onSelect={handleFilterDefault}
+            onChange={handleFilterDefault}
+            onClick={handleFilterDefault}
+          >
             Default
           </option>
-          <option value="Most" onSelect={handleFilterASC} onTouchEnd={handleFilterASC} onClick={handleFilterASC}>
+          <option
+            value="Most"
+            onSelect={handleFilterASC}
+            onChange={handleFilterASC}
+            onClick={handleFilterASC}
+          >
             Most
           </option>
-          <option value="Least" onSelect={handleFilterDESC} onTouchEnd={handleFilterDESC} onClick={handleFilterDESC}>
+          <option
+            value="Least"
+            onSelect={handleFilterDESC}
+            onChange={handleFilterDESC}
+            onClick={handleFilterDESC}
+          >
             Least
           </option>
         </optgroup>

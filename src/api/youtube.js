e32ef176bc9800bf9cad8228d8d1532f44ad9010
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-18 20:41:24 +0300

    api rotate

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const getRandomAPI = () => {
-  // const APIsUSTD = [
-  //   'AIzaSyDFxK1zpsZJ2x_MmEUtiqUYuk6k8NPfLUM',
-  //   'AIzaSyBCuAiUJs55mQy4fl9ycACFTwrytY8qGcY',
-  //   'AIzaSyDkEjQrUUV8eXp5KNhuZRAAh0zktDdl5lE',
-  //   'AIzaSyB9vXmreuu8fhJWoB8hYlsQW4j7PdUsjdc',
-  //   'AIzaSyBNgl5w_NLUnAVU52K9qY5GMeVOnx7nHbE',
-  // ];
+  const APIsUSTD = [
+    'AIzaSyDFxK1zpsZJ2x_MmEUtiqUYuk6k8NPfLUM',
+    'AIzaSyBCuAiUJs55mQy4fl9ycACFTwrytY8qGcY',
+    'AIzaSyDkEjQrUUV8eXp5KNhuZRAAh0zktDdl5lE',
+    'AIzaSyB9vXmreuu8fhJWoB8hYlsQW4j7PdUsjdc',
+    'AIzaSyBNgl5w_NLUnAVU52K9qY5GMeVOnx7nHbE',
+  ];
 
   // const YTN = [
   //   'AIzaSyDz9l33GwPNVEOPrLLm9c-q6xa_pU0wzQo',
@@ -17,36 +17,43 @@ const getRandomAPI = () => {
   //   'AIzaSyB5VtykiWl0YPGLrahY5XsF39JcjaOE7W8',
   // ];
 
-  const newQuota = [
-    'AIzaSyDonqbkYeWFLc6_BW3THeUj1tEs90_LKOY',
-    'AIzaSyBRsYuLbfYsBsl3B0oGCKz07KNffOwvTDs',
-    'AIzaSyCVdRzgM5kZFS-YOF67eAizPRq5H0LlPRc',
-    'AIzaSyDuNMiA_gxJBd5Ta2Lcc2DSX5CxiNjTFpk',
-    'AIzaSyCkfLsl_nQnEPZYgB_r_Vw8jHT2PtGvAGI',
-  ];
-  return  newQuota[Math.floor(Math.random( newQuota.length) *  newQuota.length)];
+  // const newQuota = [
+  //   'AIzaSyDonqbkYeWFLc6_BW3THeUj1tEs90_LKOY',
+  //   'AIzaSyBRsYuLbfYsBsl3B0oGCKz07KNffOwvTDs',
+  //   'AIzaSyCVdRzgM5kZFS-YOF67eAizPRq5H0LlPRc',
+  //   'AIzaSyDuNMiA_gxJBd5Ta2Lcc2DSX5CxiNjTFpk',
+  //   'AIzaSyCkfLsl_nQnEPZYgB_r_Vw8jHT2PtGvAGI',
+  // ];
+  return APIsUSTD[Math.floor(Math.random(APIsUSTD.length) * APIsUSTD.length)];
 };
 
+export const source = axios.CancelToken.source();
+
 export const youtube = axios.create({
-  baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=US&key=${getRandomAPI()}`,
+  baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${getRandomAPI()}`,
+  cancelToken: source.token,
 });
 
 export const youtubeVideo = (videoId) =>
   axios.create({
     baseURL: `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${getRandomAPI()}`,
+    cancelToken: source.token,
   });
 
 export const relatedVideos = (videoId) =>
   axios.create({
     baseURL: `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&maxResults=20&type=video&key=${getRandomAPI()}`,
+    cancelToken: source.token,
   });
 
 export const searchVideo = (keyword) =>
   axios.create({
     baseURL: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${keyword}&key=${getRandomAPI()}`,
+    cancelToken: source.token,
   });
 
 export const avatar = (id) =>
   axios.create({
     baseURL: `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${id}&key=${getRandomAPI()}`,
+    cancelToken: source.token,
   });

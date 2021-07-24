f34f33941cbc9967ad13128173747074e91dcd96
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-07-24 14:24:10 +0100

    small changes / new custom hook for input search

--- src/components/SearchInputHeader/SearchInputHeader.jsx
+++ src/components/SearchInputHeader/SearchInputHeader.jsx
@@ -1,62 +1,20 @@
-import React, { useState, useMemo, useEffect, useCallback, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import debounce from 'lodash.debounce';
-import prediction from 'helpers/searchPrediction';
+import React from 'react';
+import useSearchInput from 'hooks/useSearchInput';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { HomeVideosContext } from 'context/HomeVideosContext';
 
 import styles from './SearchInputHeader.module.css';
 
 function SearchInputHeader() {
-  const history = useHistory();
-  const { setKeyword } = useContext(HomeVideosContext);
-
-  const [input, setInput] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
-  const [visible, setVisible] = useState(false);
-
-  const eventOnBlur = (e) => {
-    if (!e.currentTarget.contains(e.relatedTarget)) {
-      setVisible((prev) => !prev);
-    }
-  };
-
-  // SET INPUT ON CHANGE
-  const onInputChange = useCallback(
-    (e) => {
-      e.preventDefault();
-      setInput(e.target.value);
-      setKeyword(e.target.value.toLowerCase());
-
-      // GET PREDICTIONS
-      const onTypeSuggest = async () => {
-        const response = await prediction(input);
-        setSuggestions(response);
-      };
-
-      onTypeSuggest();
-    },
-    [input, setKeyword],
-  );
-
-  // INPUT SUBMIT / BUTTON SUBMIT
-  const onInputSubmit = (e) => {
-    e.preventDefault();
-    history.push(`results?search=${input}`);
-  };
-
-  // Debounce for search input
-  const debouncedInputChanged = useMemo(() => debounce(onInputChange, 200), [onInputChange]);
-  useEffect(() => () => debouncedInputChanged.cancel(), [debouncedInputChanged]);
-
-  // ONSELECTED PREDICTION SUBMIT I WANT TO PUSH SELECTED ITEM INTO HISTORY ADDRESS
-  // e prevent default on submit
-  // I want to set the sugggestion if you click inside the input change suggestions
-  const onSelectedPredictionSubmit = (e) => {
-    e.preventDefault();
-    history.push(`results?search=${e.target.textContent}`.replace(/ /g, '+'));
-    setVisible((prev) => !prev);
-  };
+  const {
+    eventOnBlur,
+    onInputSubmit,
+    debouncedInputChanged,
+    setVisible,
+    visible,
+    suggestions,
+    onSelectedPredictionSubmit,
+    input,
+  } = useSearchInput();
 
   return (
     <div className={styles.headerContainerSearch}>
--- src/hooks/useSearchInput.js
+++ src/hooks/useSearchInput.js
@@ -1,82 +1,70 @@
-import { useState } from 'react';
-import prediction from 'helpers/searchPrediction';
+import { useState, useEffect, useCallback, useContext, useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
-// import { HomeVideosContext } from 'context/HomeVideosContext';
+import debounce from 'lodash.debounce';
+import prediction from 'helpers/searchPrediction';
+import { HomeVideosContext } from 'context/HomeVideosContext';
 
 const useSearchInput = () => {
   const history = useHistory();
-  // const { setKeyword } = useContext(HomeVideosContext);
+  const { setKeyword } = useContext(HomeVideosContext);
 
   const [input, setInput] = useState('');
-  const [suggestions, setSuggestions] = useState(null);
+  const [suggestions, setSuggestions] = useState([]);
+  const [visible, setVisible] = useState(false);
 
-  const onInputChange = async (e) => {
-    e.preventDefault();
-    setInput(e.target.value);
-    const predictionResponse = await prediction(e.target.value);
-    setSuggestions(predictionResponse);
+  const eventOnBlur = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setVisible((prev) => !prev);
+    }
   };
 
-  const onInputSubmit = () => {
+  // SET INPUT ON CHANGE
+  const onInputChange = useCallback(
+    (e) => {
+      e.preventDefault();
+      setInput(e.target.value);
+      setKeyword(e.target.value.toLowerCase());
+
+      // GET PREDICTIONS
+      const onTypeSuggest = async () => {
+        const response = await prediction(input);
+        setSuggestions(response);
+      };
+
+      onTypeSuggest();
+    },
+    [input, setKeyword],
+  );
+
+  // INPUT SUBMIT / BUTTON SUBMIT
+  const onInputSubmit = (e) => {
+    e.preventDefault();
     history.push(`results?search=${input}`);
   };
 
-  // const onTypeSuggest = async () => {
-  //   const response = await prediction(input);
-  //   setSuggestions(response);
-  // };
+  // Debounce for search input
+  const debouncedInputChanged = useMemo(() => debounce(onInputChange, 200), [onInputChange]);
+  useEffect(() => () => debouncedInputChanged.cancel(), [debouncedInputChanged]);
 
-  return { onInputSubmit, onInputChange, input, suggestions };
+  // ONSELECTED PREDICTION SUBMIT I WANT TO PUSH SELECTED ITEM INTO HISTORY ADDRESS
+  // e prevent default on submit
+  // I want to set the sugggestion if you click inside the input change suggestions
+  const onSelectedPredictionSubmit = (e) => {
+    e.preventDefault();
+    history.push(`results?search=${e.target.textContent}`.replace(/ /g, '+'));
+    setVisible((prev) => !prev);
+  };
+
+  return {
+    onSelectedPredictionSubmit,
+    onInputSubmit,
+    eventOnBlur,
+    suggestions,
+    visible,
+    debouncedInputChanged,
+    setVisible,
+    input,
+  };
 };
 
 export default useSearchInput;
-
-// const history = useHistory();
-// const { setKeyword } = useContext(HomeVideosContext);
-
-// const [input, setInput] = useState('');
-// const [suggestions, setSuggestions] = useState([]);
-// const [visible, setVisible] = useState(false);
-
-// const eventOnBlur = (e) => {
-//   if (!e.currentTarget.contains(e.relatedTarget)) {
-//     setVisible((prev) => !prev);
-//   }
-// };
-
-// // SET INPUT ON CHANGE
-// const onInputChange = useCallback(
-//   (e) => {
-//     e.preventDefault();
-//     setInput(e.target.value);
-//     setKeyword(e.target.value.toLowerCase());
-
-//     // GET PREDICTIONS
-//     const onTypeSuggest = async () => {
-//       const response = await prediction(input);
-//       setSuggestions(response);
-//     };
-
-//     onTypeSuggest();
-//   },
-//   [input, setKeyword],
-// );
-
-// // INPUT SUBMIT / BUTTON SUBMIT
-// const onInputSubmit = (e) => {
-//   e.preventDefault();
-//   history.push(`results?search=${input}`);
-// };
-
-// // Debounce for search input
-// const debouncedInputChanged = useMemo(() => debounce(onInputChange, 200), [onInputChange]);
-// useEffect(() => () => debouncedInputChanged.cancel(), [debouncedInputChanged]);
-
-// // ONSELECTED PREDICTION SUBMIT I WANT TO PUSH SELECTED ITEM INTO HISTORY ADDRESS
-// // e prevent default on submit
-// // I want to set the sugggestion if you click inside the input change suggestions
-// const onSelectedPredictionSubmit = (e) => {
-//   e.preventDefault();
-//   history.push(`results?search=${e.target.textContent}`.replace(/ /g, '+'));
-//   setVisible((prev) => !prev);
-// };
